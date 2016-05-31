f70ab08abf32331889ac58d5e3eedce36d49fcb2
Arnhav Iyengar <arnhavcam@gmail.com>
2016-05-31 11:55:54 -0500

    Initial Checkin

---
+++ app/app/src/main/java/com/chapresearch/ftcchaprfcs/FCSBLEScanner.java
@@ -0,0 +1,166 @@
+package com.chapresearch.ftcchaprfcs;
+
+import android.bluetooth.BluetoothDevice;
+import java.util.BitSet;
+
+/**
+ * Created by IyengarArnhav on 4/20/2016.
+ */
+public class FCSBLEScanner {
+
+    public enum AllianceColor {
+        RED, BLUE, NONE
+    }
+
+    public enum RunMode {
+        ON_DECK, READY, MATCH, NONE
+    }
+
+    public enum MatchCommand {
+        NONE,          // blank command
+        AUTO_INIT,
+        AUTO_START,
+        TELEOP_INIT,
+        TELEOP_START,
+        ENDGAME_START,
+        STOP
+    }
+
+    public BitSet          bitSet;          // BitSet object allows a byte array to broken down into bits
+    public av1String       getName;         // The object to decode the 6 bit ascii
+    public byte[]          bits;            // this is where the all of the bits in the scanRecord will be stored
+    public String          myName;          // name of robot controller (team number)
+    public boolean         is_ChapFCS;      // true if it is a good ChapFCS packet
+    public BluetoothDevice device;
+    public int             rssi;            // power measurement relieved from broadcast
+    public boolean         is_connectable;  // true if the incoming broadcast is connectable
+    public RunMode         mode;            // the current protocol mode that the broadcast sets
+    public String          name;            // the name of the field that the broadcast sets
+    public int             matchNumber;     // the current match number that the console sent
+    public AllianceColor   color;           // the alliance color
+    public int             position;        // position in the alliance ( 1 or 2 )
+    public boolean         is_inNextMatch;  // true if broadcast contains robot number for next match
+    public boolean         is_invited;      // true if I have a ready mode connection invite
+    public MatchCommand    command;         // the command for me in match mode
+
+    public FCSBLEScanner (final BluetoothDevice device, int rssi, byte[] scanRecord, String myTeamNumber){
+
+        this.device = device;
+        this.rssi = rssi;
+        this.myName = myTeamNumber;
+        this.bitSet = BitSet.valueOf(scanRecord);
+        this.bits = bitSet.toByteArray();
+        this.matchNumber = 0;
+        this.position = 0;
+        this.is_inNextMatch = false;
+        this.is_invited = false;
+        this.mode = getMode(scanRecord);
+        this.color = AllianceColor.NONE;
+        this.command = MatchCommand.NONE;
+
+        // determine if this is a connectable broadcast or not
+
+        this.is_connectable = (int)scanRecord[2] != 4;
+
+        // set the inital index for scanning the record
+
+        int index = 5;
+
+        // check for the magic value in the scan record to ensure it is a ChapFCS packet
+
+        this.is_ChapFCS = (char)scanRecord[index] == 'C' && (char)scanRecord[index+1] == 'F';
+
+        // get the name from the broadcast
+
+        this.name = getName(scanRecord);
+
+        // get the match number fro the broadcast
+
+        this.matchNumber = getMatch(scanRecord);
+    }
+
+    public RunMode getMode (byte[] bytes){
+        if (bytes[7] == 0)
+            return RunMode.ON_DECK;
+        else if (bytes[7] == 1)
+            return RunMode.READY;
+        else if (bytes[7] == 2)
+            return RunMode.MATCH;
+        return RunMode.NONE;
+    }
+
+    public String getName(byte[] bytes){
+        byte[] temp = new byte[9];
+        for (int i = 8, k = 0; i < 17; i++, k++){
+            temp[k] = bytes[i];
+        }
+        this.getName = new av1String(temp, 12);
+
+        return getName.toString();
+    }
+
+    public int getMatch(byte[] bytes){
+        int match = 0;
+        for (int i = 17; i < 18; i++){
+            match = (int) bytes[i];
+        }
+        return match;
+    }
+
+    public boolean readyMode(byte[] bytes){
+        int R1 = 0, R2 = 0, B1 = 0, B2 = 0;
+        String r1, r2, b1, b2;
+        for (int i = 21; i < 23; i++){
+            R1 += (int)bytes[i];
+        }
+        for (int i = 23; i < 25; i++){
+            R2 += (int)bytes[i];
+        }
+        for (int i = 25; i < 27; i++){
+            B1 += (int)bytes[i];
+        }
+        for (int i = 27; i < 29; i++){
+            B2 += (int)bytes[i];
+        }
+
+        r1 = R1 + "";
+        r2 = R2 + "";
+        b1 = B1 + "";
+        b2 = B2 + "";
+
+        if (myName.equals(r1) || myName.equals(r2)){
+            color = AllianceColor.RED;
+        }
+        else if (myName.equals(b1)|| myName.equals(b2)){
+            color = AllianceColor.BLUE;
+        }
+        else {
+            color = AllianceColor.NONE;
+        }
+
+        if (myName.equals(r1)|| myName.equals(b1)){
+            position = 1;
+        }
+        else if (myName.equals(r2) || myName.equals(b2)){
+            position = 2;
+        }
+        else {
+            position = 0;
+        }
+
+        if (myName.equals(r1)|| myName.equals(r2) || myName.equals(r1)|| myName.equals(r2))
+            return true;
+        return false;
+    }
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
---
+++ app/app/src/main/java/com/chapresearch/ftcchaprfcs/FCSMainActivity.java
@@ -0,0 +1,170 @@
+package com.chapresearch.ftcchaprfcs;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chapresearch.ftcchaprfcs.av1String;
+
+import java.util.Arrays;
+
+public class FCSMainActivity extends ActionBarActivity {
+
+    private Button confirmButton;
+    private Button scanButton;
+    private Button backButton;
+    private Spinner autoSelector;
+    private Spinner teleopSelector;
+    private Spinner fieldOptions;
+    private TextView fieldText;
+    private TextView autoText;
+    private TextView teleopText;
+    private TextView messageText;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private boolean bScanning;
+    private Handler bHandler;
+    private static final long SCAN_PERIOD = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fcsmain);
+
+        confirmButton = (Button) findViewById(R.id.confirmButton);
+        scanButton = (Button) findViewById(R.id.scanButton);
+        backButton = (Button) findViewById(R.id.backButton);
+        autoSelector = (Spinner) findViewById(R.id.autoPicker);
+        teleopSelector = (Spinner) findViewById(R.id.teleOpPicker);
+        fieldOptions = (Spinner) findViewById(R.id.fieldOptions);
+        fieldText = (TextView) findViewById(R.id.fieldText);
+        autoText = (TextView) findViewById(R.id.autoText);
+        teleopText = (TextView) findViewById(R.id.teleopText);
+        messageText = (TextView) findViewById(R.id.errorMessage);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        bHandler=new Handler();
+        bScanning = true;
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this,"bluetooth hardware not found :(", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                scanButton.setVisibility(View.INVISIBLE);
+                confirmButton.setVisibility(View.INVISIBLE);
+                autoSelector.setVisibility(View.INVISIBLE);
+                teleopSelector.setVisibility(View.INVISIBLE);
+                fieldText.setVisibility(View.INVISIBLE);
+                autoText.setVisibility(View.INVISIBLE);
+                teleopText.setVisibility(View.INVISIBLE);
+                messageText.setVisibility(View.VISIBLE);
+                backButton.setVisibility(View.VISIBLE);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                scanButton.setVisibility(View.VISIBLE);
+                confirmButton.setVisibility(View.VISIBLE);
+                autoSelector.setVisibility(View.VISIBLE);
+                teleopSelector.setVisibility(View.VISIBLE);
+                fieldText.setVisibility(View.VISIBLE);
+                autoText.setVisibility(View.VISIBLE);
+                teleopText.setVisibility(View.VISIBLE);
+                messageText.setVisibility(View.INVISIBLE);
+                backButton.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //scanLeDevice(true);
+                mBluetoothAdapter.startLeScan(bLeScanCallback);
+            }
+        });
+    }
+
+    private BluetoothAdapter.LeScanCallback bLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+                    FCSBLEScanner record = new FCSBLEScanner(device, rssi, scanRecord, mBluetoothAdapter.getName());
+                    if (record.is_ChapFCS){
+                        Log.d("Address", device.getAddress());
+                        Log.d("RSSI",Integer.toString(rssi));
+                        Log.d("SR",record.bytesToHex(scanRecord));
+                        Log.d("Bits", Arrays.toString(record.bits));
+                        switch (record.mode){
+                            case ON_DECK:
+                                if (record.is_connectable){
+
+                                }
+                                break;
+                            case READY:
+                                if (record.is_inNextMatch){
+                                    messageText.setTextColor(Color.GREEN);
+                                    messageText.setText("Access: Granted");
+                                }
+                                else {
+                                    messageText.setTextColor(Color.RED);
+                                    messageText.setText("Access: Denied");
+                                }
+                                break;
+                            case MATCH:
+                                break;
+                        }
+                    }
+                    while (record.mode == FCSBLEScanner.RunMode.READY || record.mode == FCSBLEScanner.RunMode.MATCH){
+                        mBluetoothAdapter.startLeScan(bLeScanCallback);
+                    }
+                }
+
+            };
+
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            bHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    bScanning = false;
+                    mBluetoothAdapter.stopLeScan(bLeScanCallback);
+                    invalidateOptionsMenu();
+                }
+            }, SCAN_PERIOD);
+            bScanning = true;
+            mBluetoothAdapter.startLeScan(bLeScanCallback);
+        } else {
+            bScanning = false;
+            mBluetoothAdapter.stopLeScan(bLeScanCallback);
+        }
+        invalidateOptionsMenu();
+    }
+}
