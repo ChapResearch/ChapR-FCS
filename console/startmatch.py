2498a98a4157279341159fba33668f4654e002ce
Jacob Rothfus <jacob@rothfus.com>
2016-06-04 13:48:33 -0500

    Continued edditing, added a stopMatch method that allows stopping and restarting of match.

--- console/startmatch.py
+++ console/startmatch.py
@@ -42,6 +42,8 @@ class StartMatchScreen(Screen):
         self.teleopStopped = False
         self.endGame = False
         self.matchEnd = False
+        self.matchSection = "Initialize"
+        self.currentClockVal = 150
 
         self.Button = self.buttons(bgcolor = (0,0,255), rock = "NW",fontsize = 25,
                                      **Button.standardButton("NW",str(match.getTeam(Match.B1).getNumber()),self.screen))
@@ -57,16 +59,33 @@ class StartMatchScreen(Screen):
     def done(self):
         return "back"
 
+    def stopMatch(self):
+        self.currentClockVal = self.clock.time
+        self.clock.stop()
+        self.bigScreen.clockStop()
+        self.autoRunning = False
+        self.teleopRunning = False
+        if self.matchSection == "Autonomous":
+            self.ButtonS.callback = self.startAutonomous
+        elif self.matchSection == "Teleop":
+            self.ButtonS.callback = self.startTeleop
+
     def startTeleop(self):
         self.autoStopped = False
         self.teleopStart = True
         self.soundFX = pygame.mixer.Sound(os.path.join('Media','StartTeleop.wav'))
         self.soundFX.play()
+        self.ButtonS.setLabels(["Stop","Teleop"])
+        self.matchSection = "Teleop"
+        self.ButtonS.callback = self.stopMatch()
 
     def startAutonomous(self):
         self.initializeStopped = False
         self.autoRunning = True
         self.soundFX.play()
+        self.ButtonS.setLabels(["Stop","Autonomous"])
+        self.matchSection = "Autonomous"
+#        self.ButtonS.callback = self.stopMatch()
 
     def _enter(self):
         self.clock.setTime(2,30)
@@ -87,6 +106,8 @@ class StartMatchScreen(Screen):
            
         # Start Autonomous
         if self.autoRunning:
+            self.clock.setTime(self.currentClockVal/60,self.currentClockVal%60)
+            self.bigScreen.clockSet(self.currentClockVal)
             self.clock.run()
             self.bigScreen.clockRun()
             self.clock.update()
@@ -110,6 +131,8 @@ class StartMatchScreen(Screen):
 
         # Start TELEOP
         if self.teleopStart:
+            self.clock.setTime(self.currentClockVal/60,self.currentClockVal%60)
+            self.bigScreen.cloclSet(self.currentClockVal)
             self.clock.run()
             self.bigScreen.clockRun()
             self.clock.update()
