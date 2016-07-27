3bbcbbfe1f2cd96249894b2b28a9f792e7daa338
Jacob Rothfus <jacob@rothfus.com>
2016-07-27 11:18:35 -0500

    HUGE update! Started work on startmatch screen, by this point stats dislay, clock displays on both screens, sections and stoping and starting of match work, settings from sonfig file are used, and teams from match object are used

--- console/startmatch.py
+++ console/startmatch.py
@@ -14,18 +14,22 @@ from HDMIclock import HDMIClock
 from globalVariables import RED,GREEN,BLUE,YELLOW,BLACK,WHITE
 from Team import Match
 import os
+from statsdisplay import StatsDisplay
+from settings import Settings
+from matchsetup import PrepareMatchScreen
 
 class StartMatchScreen(Screen):
 
     #self.matchState
-    INIT = 0     #    INIT - the state in which robots initialize
-    INITsT = 1   #    INITsT - inbetween INIT and AUTO state
-    AUTO = 2     #    AUTO - The state in which autonomous runs
-    AUTOsT = 3   #    AUTOsT - inbetween state of AUTO and TELEOP
-    TELEOP = 4   #    TELEOP - teleop runs
-    END = 5      #    END - endgame
-    TELEOPsT = 6 #    TELEOPsT - end of match state
-    STOP = 7     #    STOP - pauses match
+    ENTER = 0    #
+    INIT = 1     #    INIT - the state in which robots initialize
+    INITsT = 2   #    INITsT - inbetween INIT and AUTO state
+    AUTO = 3     #    AUTO - The state in which autonomous runs
+    AUTOsT = 4   #    AUTOsT - inbetween state of AUTO and TELEOP
+    TELEOP = 5   #    TELEOP - teleop runs
+    END = 6      #    END - endgame
+    TELEOPsT = 7 #    TELEOPsT - end of match state
+    STOP = 8     #    STOP - pauses match
 
     def __init__(self,name,match,bigScreen):
         Screen.__init__(self,name)
@@ -38,29 +42,49 @@ class StartMatchScreen(Screen):
         self.bigScreen = bigScreen
         self.bigStopped = False
         self.bigScreen.fill(0,0,0)
-#        self.BSWidth = self.get_width()
-#        self.BSHeight = self.get_height()
-#        pygame.draw.rect(self.bigScreen,(0,0,255),(0,0,self.BS/3,self.BSHeight/9),0)
-  
 
-        self.Button = self.buttons(bgcolor = (0,0,255), rock = "NW",fontsize = 25,
-                                     **Button.standardButton("NW",str(match.getTeam(Match.B1).getNumber()),self.screen))
-        self.Button = self.buttons(bgcolor = (255,0,0), rock = "NE",fontsize = 25,
-                                     **Button.standardButton("NE",str(match.getTeam(Match.R1).getNumber()),self.screen))
-        self.Button = self.buttons(bgcolor = (0,0,255), rock = "SW",fontsize = 25,
-                                     **Button.standardButton("SW",str(match.getTeam(Match.B2).getNumber()),self.screen))
-        self.Button = self.buttons(bgcolor = (255,0,0), rock = "SE",fontsize = 25,
-                                     **Button.standardButton("SE",str(match.getTeam(Match.R2).getNumber()),self.screen))
-        self.ButtonS = self.buttons(bgcolor = (255,255,255), callback=self.done, lcolor = (0,0,0),
-                                     **Button.standardButton("S",["Stop"],self.screen))
+        self.ButtonNW = self.buttons(bgcolor = (0,0,255), rock = "NW",fontsize = 20,
+                                     **Button.standardButton("NW",["Stop",""],self.screen))
+        self.ButtonNE = self.buttons(bgcolor = (255,0,0), rock = "NE",fontsize = 20,
+                                     **Button.standardButton("NE",["Stop",""],self.screen))
+        self.ButtonSW = self.buttons(bgcolor = (0,0,255), rock = "SW",fontsize = 20,
+                                     **Button.standardButton("SW",["Stop",""],self.screen))
+        self.ButtonSE = self.buttons(bgcolor = (255,0,0), rock = "SE",fontsize = 20,
+                                     **Button.standardButton("SE",["Stop",""],self.screen))
+        self.ButtonS = self.buttons(bgcolor = (255,255,255), callback=self.initialize, lcolor = (0,0,0),
+                                     **Button.standardButton("S",["Initialize"],self.screen))
+        self.ButtonN = self.buttons(bgcolor = (0,0,0), callback=self.endMatch, lcolor = (0,0,0),
+                                     **Button.standardButton("N","Yep",self.screen))
 
     def done(self):
+        PrepareMatchScreen.match.clear()
         return "back"
 
+    def none(self):
+        pass
+
+    def endMatch(self):
+        return "MainScreen"
+    
+    #Redraws the team numbers and stats on HDMI screen
+    #        -The stats variable determines whether stats are drawn
+    def redrawStats(self,stats=True):
+        self.bigScreen.fill(0,0,0)
+        self.bigScreen.cteam("NW","148",4,95,88,stats)
+        self.bigScreen.cteam("NE","2468",1,35,56,stats)
+        self.bigScreen.cteam("SW","5628",3,100,100,stats)
+        self.bigScreen.cteam("SE","10241",3,10,13,stats)        
+
+    #Pause Function for match
     def stopMatch(self):
         self.clock.stop()
         self.bigScreen.clockStop()
         self.matchState = StartMatchScreen.STOP
+        # Make North Button Appear
+        self.ButtonN.setLabels(["End","Match"])
+        self.ButtonN.callBack = self.endMatch
+        self.ButtonN.bgcolor = (255,255,255)
+        # Check what section of match to go back to
         if self.matchSection == StartMatchScreen.AUTO:
             self.ButtonS.callback = self.startAutonomous
             self.ButtonS.setLabels(["Start","Autonomous"])
@@ -68,61 +92,109 @@ class StartMatchScreen(Screen):
             self.ButtonS.callback = self.startTeleop
             self.ButtonS.setLabels(["Start","Teleop"])
 
+    def initialize(self):
+        self.matchState = StartMatchScreen.INIT
+        print("Initialized")
+        self.matchState - StartMatchScreen.INITsT
+        self.ButtonS.callback = self.startAutonomous
+        self.ButtonS.setLabels(["Start","Autonomous"])
+        
     def startTeleop(self):
         print("StartTeleop")
         self.matchSection = StartMatchScreen.TELEOP
         self.matchState = StartMatchScreen.TELEOP
         self.soundFX = pygame.mixer.Sound(os.path.join('Media','StartTeleop.wav'))
         self.soundFX.play()
-        self.ButtonS.setLabels(["Stop","Teleop"])
+        self.ButtonS.setLabels(["Pause","Teleop"])
         self.clock.setTime(self.currentClockVal/60,self.currentClockVal%60)
         self.bigScreen.clockSet(self.currentClockVal/60,self.currentClockVal%60)        
         self.ButtonS.callback = self.stopMatch
+        # Make North Button Dissapear
+        self.ButtonN.setLabels("")
+        self.ButtonN.callBack = self.none
+        self.ButtonN.bgcolor = (0,0,0)
 
     def startAutonomous(self):
         self.soundFX.play()
-        self.ButtonS.setLabels(["Stop","Autonomous"])
+        self.ButtonS.setLabels(["Pause","Autonomous"])
         self.matchSection = StartMatchScreen.AUTO
         self.matchState = StartMatchScreen.AUTO
         self.clock.setTime(self.currentClockVal/60,self.currentClockVal%60)
         self.bigScreen.clockSet(self.currentClockVal/60,self.currentClockVal%60)
         self.ButtonS.callback = self.stopMatch
-        print(self.clock.time)
+        # Make North Button Dissapear
+        self.ButtonN.setLabels("")
+        self.ButtonN.callBack = self.none
+        self.ButtonN.bgcolor = (0,0,0)
 
     def _enter(self):
         print(self.clock.time)
         # Resetup all variable use to run match
         self.soundFX = pygame.mixer.Sound(os.path.join('Media','StartAuto.wav'))
-        self.matchState = StartMatchScreen.INIT
-        self.currentClockVal = 150
+        self.matchState = StartMatchScreen.ENTER
+        self.currentClockVal = Settings.autoTime + Settings.teleopTime 
 
-        self.clock.setTime(2,30)
-        self.bigScreen.clockSet(2,30)
+        #setup clock
+        self.clock.setTime(self.currentClockVal/60,self.currentClockVal%60)
+        self.bigScreen.clockSet(self.currentClockVal/60,self.currentClockVal%60)
+        self.clock.setColor(YELLOW)
+        self.bigScreen.clockColor(YELLOW)
+
+        #Draw teams and stats on HDMI screen
+        self.updateTeams()
+        self.bigScreen.cteam("NE",self.teamR1,True,75,21)
+        self.bigScreen.cteam("SE",self.teamR2,False,35,61)
+        self.bigScreen.cteam("NW",self.teamB1,True,99,100)
+        self.bigScreen.cteam("SW",self.teamB2,True,10,13)
+        print("Updated teams: R1")
+        print("\""+self.teamR1+"\"")
         # Start initilization once screen opens            
-        self.matchState = StartMatchScreen.INITsT
+
+    def updateTeams(self):
+
+        if self.match.getTeam(Match.B1) is not None:
+            self.teamB1 = str(self.match.getTeam(Match.B1).getNumber())
+        else:
+            self.teamB1 = ""
+        if self.match.getTeam(Match.R1) is not None:
+            self.teamR1 = str(self.match.getTeam(Match.R1).getNumber())
+        else:
+            self.teamR1 = ""
+        if self.match.getTeam(Match.B2) is not None:
+            self.teamB2 = str(self.match.getTeam(Match.B2).getNumber())
+        else:
+            self.teamB2 = ""
+        if self.match.getTeam(Match.R2) is not None:
+            self.teamR2 = str(self.match.getTeam(Match.R2).getNumber())
+        else:
+            self.teamR2 = ""
+
+        self.ButtonNW.setLabels(["Stop",self.teamB1])
+        self.ButtonNE.setLabels(["Stop",self.teamR1])
+        self.ButtonSW.setLabels(["Stop",self.teamB2])
+        self.ButtonSE.setLabels(["Stop",self.teamR2])
 
     def _process(self):
+
+        self.updateTeams()
+
         # If the end of initilization has been reached
-        if self.matchState == StartMatchScreen.INITsT:
-            self.ButtonS.callback = self.startAutonomous
-            self.ButtonS.setLabels(["Start","Autonomous"])
+#        if self.matchState == StartMatchScreen.INITsT:
            
         # Start Autonomous
         if self.matchState == StartMatchScreen.AUTO:
-            print(self.clock.time)
             self.clock.run()
             self.bigScreen.clockRun()
             self.clock.update()
-            print(self.clock.time)
-            if self.clock.time == 119:
-                self.clock.setColor(BLUE)
-                self.bigScreen.clockColor(BLUE)
-            if not self.bigStopped and self.clock.time == 120:
+            if self.clock.time == Settings.teleopTime:
+                self.clock.setColor(GREEN)
+                self.bigScreen.clockColor(GREEN)
+            if not self.bigStopped and self.clock.time == Settings.teleopTime:
                 self.currentClockVal = self.clock.time
                 self.bigScreen.clockStop()
                 self.bigScreen.clockSet(self.clock.time) # make sure in sync
                 self.clock.stop()
-                self.bigStopped = True
+#                self.bigStopped = True
                 self.soundFX = pygame.mixer.Sound(os.path.join('Media','EndAuto.wav'))
                 self.soundFX.play()
                 self.matchState = StartMatchScreen.AUTOsT
@@ -131,6 +203,9 @@ class StartMatchScreen(Screen):
         if self.matchState == StartMatchScreen.AUTOsT:
             self.ButtonS.callback = self.startTeleop
             self.ButtonS.setLabels(["Start","Teleop"])
+            self.ButtonN.setLabels(["End","Match"])
+            self.ButtonN.callBack = self.endMatch
+            self.ButtonN.bgcolor = (255,255,255)
 
         # Start TELEOP
         if self.matchState == StartMatchScreen.TELEOP:
@@ -139,16 +214,25 @@ class StartMatchScreen(Screen):
             self.bigScreen.clockRun()
 #            print(self.clock.time)
             self.clock.update()
-            if self.clock.time == 30:
-                self.clock.setColor(GREEN)
-                self.bigScreen.clockColor(GREEN)
+            if self.clock.time == Settings.endGameTime:
+                self.clock.setColor(RED)
+                self.bigScreen.clockColor(RED)
                 self.soundFX = pygame.mixer.Sound(os.path.join('Media','StartEndGame.wav'))
                 self.soundFX.play()
             if not self.bigStopped and self.clock.time == 0:
                 self.bigScreen.clockSet(self.clock.time) # make sure in sync
                 self.soundFX = pygame.mixer.Sound(os.path.join('Media','EndTeleop.wav'))
                 self.soundFX.play()
-        
+                self.matchState = StartMatchScreen.TELEOPsT
+
+        # If Match Has Ended
+        if self.matchState == StartMatchScreen.TELEOPsT:
+            self.ButtonN.setLabels(["Match","Over"])
+            self.ButtonN.callback = self.none
+            self.ButtonN.bgcolor = (255,255,255)
+            self.ButtonS.setLabels(["End","Game"])
+            self.ButtonS.callback = self.endMatch
+
         # Stop State
         if self.matchState == StartMatchScreen.STOP:
             self.currentClockVal = self.clock.time
