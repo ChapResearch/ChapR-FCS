870ca725076649d9e316bb24cf74008cf065f1cb
Jacob Rothfus <jacob@rothfus.com>
2016-07-27 10:56:01 -0500

    Made table diplay settings from config file, made keyboard save and change field name

--- console/systemoptionsscreen.py
+++ console/systemoptionsscreen.py
@@ -30,11 +30,15 @@ from tables import Table
 from utils import textOutline, numberDraw
 import globalVariables
 from globalVariables import RED,GREEN,BLUE,YELLOW
+import virtkeyboard
+from settings import Settings
 
 class SystemOptionsScreen(Screen):
-
+    
     def __init__(self,name):
         Screen.__init__(self,name)
+        
+        
 
         self.tablePosition = (20,150)
 
@@ -47,19 +51,26 @@ class SystemOptionsScreen(Screen):
         self.ButtonSW = self.buttons(bgcolor = (255,0,0), callback=self.tests,
                                      **Button.standardButton("SW","Tests",self.screen))
         self.ButtonSE = self.buttons(bgcolor = (255,0,0), callback=self.networkInternet,
-                                     **Button.standardButton("SE",["Network &","Internet"],self.screen))
+                                     **Button.standardButton("SE",["Network &","WiFi"],self.screen))
         self.ButtonS = self.buttons(bgcolor = (255,255,255), callback=self.done, lcolor=(0,0,0),
                                      **Button.standardButton("S","Done",self.screen))
 
 
     def setFieldName(self):
-        pass
+        text = Settings.fieldName
+        mykeyboard = virtkeyboard.VirtualKeyboard()
+        userinput = mykeyboard.run(pygame.display.get_surface(), 200, text)
+        print "got user input of: " + userinput
+        Settings.fieldName = userinput
+        Settings.saveSettings()
+        print(userinput)
+        self.dataTable.changeData("fieldName",data = Settings.fieldName)
 
     def hdmiAudio(self):
         pass
 
     def tests(self):
-        pass
+        return "SystemTests"
 
     def done(self):
         return "back"
@@ -71,10 +82,7 @@ class SystemOptionsScreen(Screen):
         self.dataTable = self.tables(fontsize=20,font="monospace")
 
         self.dataTable.addData("Field Name: ",align="right")
-        self.dataTable.addData("Galileo", bold=True, name="fieldName",bgcolor=(50,50,50))
-        self.dataTable.endRow()
-        self.dataTable.addData("Waiting Robots: ", align="right")
-        self.dataTable.addData("4",name="robots",bold=True)
+        self.dataTable.addData(Settings.fieldName, bold=True, name="fieldName",bgcolor=(50,50,50))
         self.dataTable.endRow()
         self.dataTable.addData("Next Match: ", align="right")
         self.dataTable.addData("10",name="match",bold=True)
@@ -89,5 +97,7 @@ class SystemOptionsScreen(Screen):
 
     def _process(self):
         pass
+
+    
 
 
