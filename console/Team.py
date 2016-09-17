3282438bd216795c69cdca1357cb3029f2428833
Eric Rothfus <eric@rothfus.com>
2016-09-17 11:05:56 -0500

    added robot lists

--- console/Team.py
+++ console/Team.py
@@ -9,6 +9,13 @@ class Team(object):
     def getNumber(self):
         return self.teamN
 
+#
+# The Match object stores information about the upcoming match.
+#   this includes accumulating the list of teams who are "trying to join the match"
+#   and those who have been trashed from joining the match.  Note that
+#   addTeam() should be called during the process loop by all relevent
+#   routines so that incoming teams joinging aren't missed.
+
 class Match(object):
 
     R1 = 1
@@ -22,6 +29,7 @@ class Match(object):
         self.positions[Match.R2] = None
         self.positions[Match.B1] = None
         self.positions[Match.B2] = None
+        self.clearTeams()
 
     def add(self,position,teamN):        
         self.positions[position] = Team(teamN)
@@ -48,4 +56,45 @@ class Match(object):
                 return self.positions[team]
         return None
         
-    
+    #
+    # addTeam() - called as part of a mechanism to check for incoming
+    #             teams joining a match.  Note that a team that is already
+    #             part of the current teams (or in the trash) is not added
+    #             again.
+    #
+    def addTeam(self,team):
+        if team in self.currentTeams or team in self.trash or team in self.incomingTeams:
+            pass
+        else:
+            self.incomingTeams.append(team)
+
+    #
+    # trashTeam() - called to disqualify a particular team from be considered
+    #               for the current match.
+    #
+    def trashTeam(self,team):
+        if team in self.currentTeams:
+            currentTeams.remove(team)
+            trashTeams.append(team)
+
+    #
+    # emptyTrash() - put all teams BACK into the current teams.
+    #
+    def emptyTrash(self):
+        self.currentTeams.extend(self.trashTeams)
+        self.trashTeams = []
+
+    #
+    # clearTeams() - clear all teams and all trash.
+    #
+    def clearTeams(self):
+        self.currentTeams = []     # the list of teams thar are considered "current" (not trash)
+        self.trashTeams = []       # teams in the trash
+        self.incomingTeams = []    # incoming teams that need to be added when convenient
+
+    #
+    # ackIncoming() - take the incoming teams, and add them to the currentTeams
+    #                         
+    def ackIncoming(self):
+        self.currentTeams.extend(self.incomingTeams)
+        self.incomingTeams = []
